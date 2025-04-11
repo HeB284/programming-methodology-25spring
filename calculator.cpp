fce8f4cfbf96a1284a4e92154122c6ba074e261c
eunbyul <hoeuby0428@snu.ac.kr>
2025-04-11 11:36:24 +0900

    2

--- calculator.cpp
+++ calculator.cpp
@@ -2,20 +2,52 @@
 #include <stdexcept>
 
 int Calculator::add(int a, int b) {
-    return a+b;
+    if ((b > 0) && (a > INT_MAX - b)) {
+        throw std::overflow_error("integer overflow in add");
+    }
+    if ((b < 0) && (a < INT_MIN - b)) {
+        throw std::overflow_error("integer underflow in add");
+    }
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
-    return a-b;
+    if ((b < 0) && (a > INT_MAX + b)) {
+        throw std::overflow_error("integer overflow in sub");
+    }
+    if ((b > 0) && (a < INT_MIN + b)) {
+        throw std::overflow_error("integer underflow in sub");
+    }
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
-    return a*b;
+    if (a > 0) {
+        if (b > 0) {
+            if (a > INT_MAX / b)
+                throw std::overflow_error("integer overflow in mul");
+        } else {
+            if (b < INT_MIN / a)
+                throw std::overflow_error("integer underflow in mul");
+        }
+    } else {
+        if (b > 0) {
+            if (a < INT_MIN / b)
+                throw std::overflow_error("integer underflow in mul");
+        } else {
+            if ((a != 0) && (b < INT_MAX / a))
+                throw std::overflow_error("integer overflow in mul");
+        }
+    }
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
     if (b == 0) {
         throw std::invalid_argument("divide by zero");
     }
-    return a/b;
+    if ((a == INT_MIN) && (b == -1)) {
+        throw std::overflow_error("division overflow");
+    }
+    return a / b;
 }
