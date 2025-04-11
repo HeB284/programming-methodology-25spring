dd7fb6aadf2f30fadbc440abb8a64287a95ba318
eunbyul <hoeuby0428@snu.ac.kr>
2025-04-11 11:31:46 +0900

    1

--- calculator.cpp
+++ calculator.cpp
@@ -2,17 +2,20 @@
 #include <stdexcept>
 
 int Calculator::add(int a, int b) {
-    // TODO
+    return a+b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    return a-b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    return a*b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b == 0) {
+        throw std::invalid_argument("divide by zero");
+    }
+    return a/b;
 }
